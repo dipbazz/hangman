199f0e8580ea8b951d05ae38bf15ef84e8cfea96
Dipesh Bajgain <dipbazz@gmail.com>
2020-11-05 18:23:29 +0545

    Add different methods on Hangman class.
    Methods added:
    - #play, this method is where the user start playing game.
    - #ask_character, this method ask a new guess character with user.
    - #valid_character?, this method checks if the provided character is valid or not.
    - #check_character, this method checks if the user input character is in the hidden
      word or not.
    - #game_over?, checks for the available user turn and determine if the game is over
      or not.

--- main.rb
+++ main.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 words = %w[python awesome ruby rails book javascript space engineering laptop]
 
 class Hangman
@@ -11,5 +13,53 @@ class Hangman
     @over = false
   end
 
+  def play
+    # binding.pry()
+
+    character = ask_character
+
+    until self.over
+      if valid_character?(character)
+        check_character(character)
+      end
+      ask_character
+    end
+  end
+
+  def ask_character
+    puts "Enter your guess character:"
+    gets.chomp
+  end
+
+  def valid_character?(character)
+    if character.length == 1
+      return true
+    else
+      puts "#{character} is not valid please enter only one character between a-z."
+    end
+    false
+  end
+
+  def check_character(character)
+    if hidden_word.include?(character)
+      puts "yes it includes it"
+    else
+      self.turn -= 1
+      game_over?
+      puts "No this character is not here."
+    end
+  end
+
+  def game_over?
+    if self.turn < 1
+      self.over = true
+    end
+    self.over
+  end
+
 end
 
+
+player = Hangman.new(words.sample)
+
+player.play
